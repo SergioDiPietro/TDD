0277c6c5d2942ba90d1139d56ff063e84f4a4dcc
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2021-05-20 11:56:26 +0100

    Test de istanciar objeto de la clase Contador fallido

---
+++ src/TestContador.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+
+public class TestContador {
+
+    @Test
+    public void testObjetoCreado() {
+        Contador test = new Contador();
+    }
+}
